ae64c4ab481f2125c0ff113f84b301eb110faae1
Natasha Wong <>
2020-05-25 17:11:50 +0800

    Edit react-router routing to suppose gh pages url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import {
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path='/'>
             <About/>
